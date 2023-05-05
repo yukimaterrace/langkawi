5a047c295675a070e8ef1a736b85f31c66a5249f
Yuki Matsuo <yukimaterrace@gmail.com>
2023-05-05 17:37:13 +0900

    Fix: app and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   scope :api do
     get '/health_check', to: 'system#health_check'
-    
+
     post '/login', to: 'login#login'
     post '/logout', to: 'login#logout'
 
     resources :accounts
 
-    resources :users, :except => [:create, :destroy]
+    resources :users, except: %i[create destroy]
 
-    resources :user_details, :except => [:index, :show, :destroy] do
+    resources :user_details, except: %i[index show destroy] do
       post '/picture_a', to: 'user_details#upload_picture_a'
     end
-    resources :relations, :except => [:destroy]
+    resources :relations, except: [:destroy]
 
-    resources :talks, :except => [:show, :destroy]
+    resources :talks, except: %i[show destroy]
     get '/talks/rooms', to: 'talks#index_rooms'
   end
 end
