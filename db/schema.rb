e0fe696754a2aefd9fcad32fedb788afca79b27d
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2023-05-15 16:58:43 +0900

    Fix: database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_16_061738) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.integer "account_type"
     t.string "email"
