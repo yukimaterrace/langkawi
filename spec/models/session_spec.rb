c9fa8cca8f36b1180e91833082b61beba60ac3a0
Yuki Matsuo <yuki.matsuo@kinto-technologies.com>
2023-05-15 16:59:21 +0900

    Add: session test

---
+++ spec/models/session_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe Session, type: :model do
+  let(:uuid) { SecureRandom.uuid }
+  let(:user_id) { SecureRandom.uuid }
+
+  describe '.put' do
+    context 'when session does not exist' do
+      before { Session.put(uuid, user_id) }
+
+      it 'creates a new session' do
+        expect(Session.find_by(user_id: user_id)).to be_present
+      end
+
+      it 'sets the correct uuid' do
+        session = Session.find_by(user_id: user_id)
+        expect(session.uuid).to eq(uuid)
+      end
+
+      it 'sets the correct expiration' do
+        session = Session.find_by(user_id: user_id)
+        expect(session.expiration).to be > Time.current
+      end
+    end
+
+    context 'when session already exists' do
+      let(:new_uuid) { SecureRandom.uuid }
+
+      before do
+        Session.create(uuid: uuid, user_id: user_id, expiration: Session.expiration)
+        Session.put(new_uuid, user_id)
+      end
+
+      it 'does not create a new session' do
+        expect(Session.where(user_id: user_id).count).to eq(1)
+      end
+
+      it 'updates the uuid of the existing session' do
+        session = Session.find_by(user_id: user_id)
+        expect(session.uuid).to eq(new_uuid)
+      end
+    end
+  end
+
+  describe '.get' do
+    context 'when session is not expired' do
+      before do
+        Session.create(uuid: uuid, user_id: user_id, expiration: Session.expiration)
+      end
+
+      it 'returns the user_id' do
+        expect(Session.get(uuid)).to eq(user_id)
+      end
+    end
+
+    context 'when session is expired' do
+      before do
+        Session.create(uuid: uuid, user_id: user_id, expiration: 1.day.ago)
+      end
+
+      it 'returns nil' do
+        expect(Session.get(uuid)).to be_nil
+      end
+    end
+  end
+end
